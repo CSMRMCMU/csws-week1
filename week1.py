591444a9eb374fec1d39d858e2bb549f087ed9cd
R.McMurray@2021.ljmu.ac.uk <R.McMurray@2021.ljmu.ac.uk>
2022-01-25 13:23:19 +0000

    test

--- week1.py
+++ week1.py
@@ -1,3 +1,29 @@
-for x in range(10):
-    print("Hello world!" + str(x))
+message = "Hello Python world!"
+print(message)
 
+message = "Hello Python Crash Course world!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+
+favorite_language = 'python '
+favorite_language.strip()
+print(favorite_language)
+
+
+name = "Eric"
+message = ", wanna learn some Pyhton?"
+print(f"Hello {name}{message}")
